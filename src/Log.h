9edb29a894ed8dd98d0a5f01da899b198e63a936
imstylen <michael.steijlen@gmail.com>
2021-05-09 20:09:38 -0400

    Initial commit

---
+++ src/Log.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "spdlog/spdlog.h"
+
+class Log
+{
+public:
+
+	void LogExamples();
+
+};
+
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Log.h"
+
+int main(int argc, char* argv[])
+{
+
+	Log log = Log();
+	
+	log.LogExamples();
+
+
+}
